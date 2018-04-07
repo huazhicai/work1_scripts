7f3c0086e3e2b1532a88bf3018cadfedd9bffa26
huazhicai <936844218@qq.com>
2018-04-07 20:20:56 +0800

    add sync.py

--- py/read.md
+++ py/read.md
@@ -1,12 +1,18 @@
 ## 说明文档
 
-### new_synv.py
+### new_sync.py
 	note:把本地ai服务产生的在/home/recordfile下的录音tar包上到
 	远端web服务的/home/recordfile下。
 	
 	v1.0 的assert os.popen(scp_cmd) 捕捉不到Exception , 因为popen是管道
 	工作原理，shell的错误没有传过来，也就没有Exception.
 	
-	v1.1 把os.open() 替换为subprocess.check_call()  `check_call：执行命令，如果执行状态码是 0 ，则返回0，否则抛异常`
+	v1.1 把os.open() 替换为subprocess.check_call()  “check_call：执行命令，如果执行状态码是 0 ，则返回0，否则抛异常”
 	如此便能捕捉到异常写入日志了。   when='D'日志并不回滚，替换为when='MIDNIGTH'， 每天凌晨就会回滚了，
 	参考：https://www.cnblogs.com/qhlblog/archive/2017/12/07/7998454.html
+
+
+### sync.py
+	功能与new_sync.py一样，只是做了一些改进， 使用了python3的模块paramiko，paramiko.SFTPClient对象
+	日志输出交给supervisor去处理，记住日志等级在py文件中设定，supervisor后于py文件。日志按大小回滚。
+
--- py/sync.py
+++ py/sync.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding:utf-8
-# author: Seven
+# author: dainel & Seven
 
 import os
 from operator import methodcaller
@@ -9,14 +9,16 @@ from datetime import datetime
 import logging
 import smtplib
 from email.mime.text import MIMEText
-from logging.handlers import TimedRotatingFileHandler
 import sys
 import subprocess
+import paramiko
 
 try:
-    from os import scandir   #python3
+    from os import scandir
 except ImportError:
-    from scandir import scandir  # python2
+    from scandir import scandir
+# 开发模式用debug, 生产模式用info
+logging.basicConfig(level=logging.INFO, format='%(asctime)s-%(levelname)s-%(message)s')
 
 remote_ip = sys.argv[1]
 
@@ -24,13 +26,13 @@ remote_ip = sys.argv[1]
 class SyncRecord(object):
     """上传录音的程序"""
 
-    def __init__(self, logger):
+    def __init__(self):
         self.recordfile_dir = '/home/recordfile'
         self.scp_dst = 'root@{}:/home/recordfile/'.format(remote_ip)
         if not os.path.exists('/recordfile_bak'):
             os.mkdir("/recordfile_bak")
         self.mv_dst = '/recordfile_bak'
-        self.log = logger
+        self.sftp = self._sftp_client
 
     def send_email(self, sub, content):
         mailto_list = ["936844218@qq.com"]
@@ -51,8 +53,23 @@ class SyncRecord(object):
             server.close()
             return True
         except Exception as e:
-            self.log.error("Email failed:" + str(e))
+            logging.error("Email failed:" + str(e))
             return False
+
+    @property
+    def _sftp_client(self):
+        """用paramiko.SFTPClient对象在不同的服务器间传递文件"""
+        transport = paramiko.Transport(remote_ip, 22)
+        try:
+            # 使用秘钥连接
+            private_key = paramiko.RSAKey.from_private_key_file('/root/.ssh/id_rsa')
+            transport.connect(username="root", pkey=private_key)
+        except Exception as e:
+            logging.warning(str(e))
+            # 使用用户名和密码连接
+            transport.connect(username='root', password=os.getenv('PASSWORD'))
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        return sftp
 
     def sync(self):
         dir_entities = scandir(self.recordfile_dir)
@@ -60,67 +77,50 @@ class SyncRecord(object):
         dir_entities = sorted(dir_entities, key=methodcaller('inode'))
         # 遍历文件夹'/home/recordfile'内的所有文件
         for dir_entity in dir_entities:
-            # 绝对路径/home/recordfile/5ab70a173baaaafc8d609e06_1522380690.tar
-            # dir_entity.name 为 5ab70a173baaaafc8d609e06_1522380690.tar
+            path = dir_entity.path
             name = path[-3:]
-            path = dir_entity.path  
             if name == 'tar':
                 cur_time = time.time()
                 m_time = os.path.getmtime(path)
                 # 复制一分钟前的文件到远程服务器
                 if cur_time - m_time > 60:
                     try:
-                        scp_cmd = 'scp %s %s' % (path, self.scp_dst)
-                        subprocess.check_call(scp_cmd, shell=True)
-                        # os.popen(scp_cmd)
-                        self.log.info('%s has been copy to %s ' % (path, self.scp_dst))
+                        self.sftp.put(path, path)
+                        logging.info('%s has been copy to %s ' % (path, self.scp_dst))
                         # 复制成功后的文件移到备份目录中
                         try:
-                            mv_cmd = 'mv %s %s' % (path, self.mv_dst)
+                            mv_cmd = 'cp %s %s' % (path, self.mv_dst)
                             subprocess.check_call(mv_cmd, shell=True)
-                            self.log.info('%s has been moved to %s ' % (path, self.mv_dst))
+                            logging.info('%s has been moved to %s ' % (path, self.mv_dst))
                         except Exception as e:
                             # 移动文件失败，应该是根目录磁盘满了，发邮件警告
-                            self.log.error(str(e) + "move failed")
+                            logging.error(str(e) + "move failed")
                             host_name = os.popen('hostname').read().strip()
                             self.send_email(host_name, 'Failed to move %s to %s ' % (path, self.mv_dst))
                             # 根目录磁盘满了，退出进程
                             # sys.exit()
-                    except subprocess.CalledProcessError as e:
+                    except Exception as e:
                         # 复制到远程失败，发送警告邮件
-                        self.log.error(str(e))
+                        logging.error(str(e))
                         host_name = os.popen('hostname').read().strip()
                         message1 = '%s: %s, Failed transfer %s to %s \n' % (
                             datetime.now(), e, path, self.scp_dst)
-                        message2 = "程序休眠5分钟，检查网络是否通畅"
+                        message2 = "程序休眠5分钟，检查网络和服务器"
                         self.send_email(host_name, message1 + message2)
                         # 程序休眠5分钟，检查网络
                         time.sleep(300)
+                        # 连接断了，socket关闭了，所以此处要重新连接
+                        self.sftp = self._sftp_client
             else:
                 # 非tar包文件，直接移到备份目录'/recordfile_bak'
                 subprocess.check_call('mv %s %s' % (dir_entity.path, self.mv_dst), shell=True)
-                self.log.warning('%s has been move to %s' % (dir_entity.path, self.mv_dst))
+                logging.warning('%s has been move to %s' % (dir_entity.path, self.mv_dst))
 
 
 def main():
-    # 日志回滚
-    logging.basicConfig()
-    logger = logging.getLogger('logger')
-    # 每次debug, 下面调用可以调用等级高的，不可用等级低的
-    logger.setLevel(logging.DEBUG)
-    if not os.path.exists('./log'):
-        os.mkdir("./log")
-    # 创建日志处理对象，保留7天内的日志，MIDNIGHT凌晨回滚
-    timefile_handler = TimedRotatingFileHandler('log/sync.log', when='MIDNIGHT', interval=1, backupCount=7)
-    timefile_handler.suffix = "%Y-%m-%d"
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    timefile_handler.setFormatter(formatter)
-    logger.addHandler(timefile_handler)
-
-    sync_record = SyncRecord(logger)
+    sync_record = SyncRecord()
     while True:
         sync_record.sync()
-
 
 if __name__ == '__main__':
     main()
